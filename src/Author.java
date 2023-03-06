1f9ae102a89e583eecef03e34c0777b81c36da71
IVAN <luka.news.2001@mail.ru>
2023-03-06 22:10:53 +0300

    Домашнее задание: Объекты и классы - Исправлено!!!!!!

---
+++ src/Author.java
@@ -0,0 +1,14 @@
+class Author {
+    public String author;
+
+    public Author (String author){
+        this.author = author;
+    }
+
+    public String getName(){
+        return author;
+    }
+    public void setName (String author){
+        this.author = author;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -9,55 +9,5 @@ public class Main {
         System.out.println("masterAndMargarita.getName() = " + masterAndMargarita.getName());
         System.out.println("masterAndMargarita.getAuthor() = " + masterAndMargarita.getAuthor());
         System.out.println("masterAndMargarita.getPublishYear() = " + masterAndMargarita.getPublishYear());
-
-    }
-}
-class Book {
-    private String name;
-    private Author author;
-    private int publishYear;
-
-    public Book (String name, Author author, int publishYear){
-        this.name = name;
-        this.author = author;
-        this.publishYear = publishYear;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Author author) {
-        this.author = author;
-    }
-
-    public int getPublishYear() {
-        return publishYear;
-    }
-
-    public void setPublishYear(int publishYear) {
-        this.publishYear = publishYear;
-    }
-}
-class Author {
-    public String author;
-
-    public Author (String author){
-        this.author = author;
-    }
-
-    public String getName(){
-        return author;
-    }
-    public void setName (String author){
-        this.author = author;
     }
 }
